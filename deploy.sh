c51393c68d6cc0ec328b1ebb6bb0b90212a67ead
Vishnu-v-007 <vardhanreddy464@gmail.com>
2025-10-07 01:22:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ this is upadted line
 hello guys
 this is deploy method
 this is updated in a branch
+this is uat branch why we need to update in dev branch hwen i udpate here because se pu;l request
